380d980833951603641487a5bf18be7ee3141685
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2025-04-11 19:06:20 +0200

    Refactor User schema to correct role enum and add settings for language, quality, and notifications

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,12 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role:     { type: String, enum: ['admin', 'normal'], default: 'normal' },
+  role:     { type: String, enum: ['admin', 'standard'], default: 'standard' },
+  settings: {
+    prefered_lang: { type: String, enum:['es','ca','en','it','de','fr','jp'],default: 'es' },
+    prefered_quality: { type: String, enum: ['720p', '1080p', '4k', 'other'], default: '1080p' },
+    notifications: { type: Boolean, default: true }
+  }
 }, {
   timestamps: true,
 });
