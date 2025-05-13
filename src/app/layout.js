b0a58c87bd558a42e0ded5630b2b8529bf6f9ef7
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2025-05-13 20:39:22 +0200

    feat: Add @vercel/analytics for enhanced analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBarWrapper from "./components/navbar/NavBarWrapper";
